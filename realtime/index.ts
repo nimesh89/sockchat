63b6e5cf73daf94c360ba84f31449ecfc5c58ef8
Nimesh Gunasekara <nimesh@bizchatbox.com>
2016-12-26 23:58:37 +0530

    redis adapter added

--- realtime/index.ts
+++ realtime/index.ts
@@ -1,8 +1,10 @@
 import {Server} from 'http'
 import * as sock from 'socket.io' 
+import * as redis from 'socket.io-redis'
 
 export function init(server: Server){
     var sockServer = sock(server)
+    sockServer.adapter(redis({ host:'localhost', port:6379 }))
     sockServer.on("connection", (socket)=>{
         sockServer.emit("newcon", JSON.stringify({ msg : "test" }))
     })
