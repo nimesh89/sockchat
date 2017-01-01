7627fa399acb294ccecc851e6086bc29be3b4e32
Nimesh Gunasekara <nimesh@bizchatbox.com>
2017-01-01 23:21:58 +0530

    communication added

--- realtime/index.ts
+++ realtime/index.ts
@@ -6,6 +6,9 @@ export function init(server: Server){
     var serv = sock(server)
     var sockServer = serv.adapter(redis({ host:'localhost', port:6379 }))
     sockServer.on("connection", (socket)=>{
-        socket.broadcast.emit("newcon", { msg : "test" })
+        socket.broadcast.emit("newcon", { msg : "connected", id: socket.id })
+        socket.on("disconnect", function(){
+            socket.broadcast.emit("conlost", { msg : "lost", id: socket.id })
+        })
     })
 }
--- wwwroot/app.js
+++ wwwroot/app.js
@@ -1,8 +1,14 @@
 $(function () {
-    var display = $("displayarea")
+    var display = $("#displayarea")
 
     var socket = io();
     socket.on('newcon', function (msg) {
         console.log(msg);
+        display.append("<div id=\""+msg.id+"\">"+msg.id+"</div")
+    });
+
+    socket.on('conlost', function (msg) {
+        console.log(msg);
+        $("#" + msg.id).remove()
     });
 })
